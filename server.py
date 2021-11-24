16dee0fa9d45f15eb4965e3ad8790c4aae09bafa
dudaskobende <dudaskobende@gmail.com>
2021-11-24 15:39:20 +0100

    Refactor login

--- server.py
+++ server.py
@@ -4,29 +4,30 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route('/login', methods=['POST', 'GET'])
 def main_page():
+    print(request.base_url)
     message = 'Create a New Account, or Log in'
     username = ''
-    if request.args:
-        if request.args['login'] == 'False':
-            message = 'Wrong Password'
-        if request.args['username']:
-            username = request.args['username']
+    if request.url_rule == '/':
+        if request.args:
+            if request.args['login'] == 'False':
+                message = 'Wrong Password'
+            if request.args['username']:
+                username = request.args['username']
+    elif '/login' in request.base_url:
+        if request.method == 'POST' and request.form['password']:
+            logged_in, valid_password, new_user = data_manager.log_in(request.form)
+            if not valid_password:
+                return redirect(f'/?login=False&username={request.form["username"]}')
+            return redirect(f'/list?username={request.form["username"]}')
     return render_template('/main_page.html', message=message, username=username)
 
 
 @app.route('/list', methods=['GET', 'POST'])
 def list_posts():
     questions = data_manager.get_questions()
-    if request.method == 'POST' and request.form['password']:
-        logged_in, valid_password, new_user = data_manager.log_in(request.form)
-        if not valid_password:
-            return redirect(f'/?login=False&username={request.form["username"]}')
-        if logged_in:
-            user = request.form["username"]
-    else:
-        return redirect(f'/?login=False&username={request.form["username"]}')
-    return render_template('list.html', user=user, questions=questions)
+    return render_template('list.html', user=request.args["username"], questions=questions)
 
 
 if __name__ == "__main__":
